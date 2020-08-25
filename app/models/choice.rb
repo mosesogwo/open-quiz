4c6447e84300ccc47c1ae62996e934edafa4f2ec
Moses Ogwo <mosesogwo@gmail.com>
2020-08-25 14:39:01 +0100

    remove presence validation for boolean fields

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,6 +1,6 @@
 class Choice < ApplicationRecord
   belongs_to :question
 
-  validates :choice, :is_right_choice, :note, presence: true
+  validates :choice, :note, presence: true
   validates :note, length: { maximum: 5000 }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :choices
 
-  validates :question, :is_active, presence: true
+  validates :question, presence: true
   validates :question, length: { minimum: 10, mazimum: 5000 }
 end
